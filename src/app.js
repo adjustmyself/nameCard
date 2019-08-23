1b07462e213fd61008e26fb4cf9ba771d0154af1
adjustmyself <adjust_myself@hotmail.com>
2019-08-23 11:56:02 +0800

    fix port-1

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-const port = process.env.POST || 3000
+const port = process.env.PORT || 3000
 // define paths for express condig
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
